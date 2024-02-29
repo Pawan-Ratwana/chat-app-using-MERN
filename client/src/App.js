a070c7929215fdba49a484c01c7d2465fd839d81
Pawan Kumar <ratwanapawan@gmail.com>
2024-02-29 10:21:07 +0530

    create client routes and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
 
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Dashboard from './modules/Dashboard';
 import Form from './modules/Form';
 
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null;
+
+  if (!isLoggedIn) {
+    return <Navigate to={'/users/sign-in'} />
+  } else if (isLoggedIn && ['/users/sign-in', '/users/sign-up'].includes(window.location.pathname)) {
+    return <Navigate to={'/'} />
+  }
+  return children;
+}
+
 function App() {
   return (
-    <div className="bg-[#9be1e6] h-screen flex justify-center items-center">
-      {/* <Form /> */}
-      <Dashboard />
-    </div>
+    <Routes>
+      <Route path='/' element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      } />
+      <Route path='users/sign-in' element={
+        <ProtectedRoute>
+          <Form isSignInPage={true} />
+        </ProtectedRoute>
+      } />
+      <Route path='users/sign-up' element={
+        <ProtectedRoute>
+          <Form isSignInPage={false} />
+        </ProtectedRoute>
+      } />
+    </Routes>
+    // <div className="bg-[#9be1e6] h-screen flex justify-center items-center">
+    //   <Form />
+
+    // </div>
   );
 }
 
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/input/input";
+import { useNavigate } from "react-router-dom";
 
 const Form = ({
     isSignInPage = false,
@@ -14,32 +15,36 @@ const Form = ({
         // confirm_password: ""
     })
 
+    const navigate = useNavigate();
+
     // console.log("data => ", data)
 
-    return (<div className="bg-white w-[600px] h-full border border-blue-500 flex flex-col items-center justify-center shadow-lg rounded-lg">
-        <div className="text-4xl font-bold mb-4">Welcome to Preetal</div>
-        <div className="text-xl font-light mb-9">{isSignInPage ? 'Sign In to get explored' : 'Sign Up now to get started'}</div>
+    return (<div className="bg-[#9be1e6] h-screen flex justify-center items-center">
+        <div className="bg-white w-[600px] h-full border border-blue-500 flex flex-col items-center justify-center shadow-lg rounded-lg">
+            <div className="text-4xl font-bold mb-4">Welcome to Preetal</div>
+            <div className="text-xl font-light mb-9">{isSignInPage ? 'Sign In to get explored' : 'Sign Up now to get started'}</div>
 
-        <form className="w-full flex flex-col  justify-center items-center" onSubmit={() => console.log("Submitted")}>
+            <form className="w-full flex flex-col  justify-center items-center" onSubmit={() => console.log("Submitted")}>
 
-            {/* input for Full name  */}
-            {!isSignInPage && <Input label="Full Name" name="name" placeholder="Enter your Full Name" className="mb-6" value={data.fullName} onChange={(e) => setData({ ...data, fullName: e.target.value })} />}
+                {/* input for Full name  */}
+                {!isSignInPage && <Input label="Full Name" name="name" placeholder="Enter your Full Name" className="mb-6" value={data.fullName} onChange={(e) => setData({ ...data, fullName: e.target.value })} />}
 
-            {/* input for email  */}
-            <Input label="Email" name="email" type="email" placeholder="Enter your Email" className="mb-6" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+                {/* input for email  */}
+                <Input label="Email" name="email" type="email" placeholder="Enter your Email" className="mb-6" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
 
-            {/* input for password  */}
-            <Input label="Password" name="password" type="password" placeholder="Enter your Password" className="mb-10" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                {/* input for password  */}
+                <Input label="Password" name="password" type="password" placeholder="Enter your Password" className="mb-10" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
 
-            {/* input for confirm password  */}
-            {/* {!isSignInPage && <Input type="password" label="Conform Password" name="confirm_password" placeholder="Enter your Confirm Password" className="mb-6" value={data.confirm_password} onChange={(e) => setData({ ...data, confirm_password: e.target.value })} />} */}
+                {/* input for confirm password  */}
+                {/* {!isSignInPage && <Input type="password" label="Conform Password" name="confirm_password" placeholder="Enter your Confirm Password" className="mb-6" value={data.confirm_password} onChange={(e) => setData({ ...data, confirm_password: e.target.value })} />} */}
 
-            {/* submit button  */}
-            <Button label={isSignInPage ? "Sign In" : "Sign Up"} type="submit" className="w-1/2 mb-3" />
+                {/* submit button  */}
+                <Button label={isSignInPage ? "Sign In" : "Sign Up"} type="submit" className="w-1/2 mb-3" />
 
-        </form>
+            </form>
 
-        <div>{isSignInPage ? "Didn't have an Account?" : "Already have an account?"} <span className="text-blue-500 cursor-pointer underline">{isSignInPage ? "Sign-up" : "Sign-in"}</span></div>
+            <div>{isSignInPage ? "Didn't have an Account?" : "Already have an account?"} <span className="text-blue-500 cursor-pointer underline" onClick={() => navigate(`/users/${isSignInPage ? 'sign-up' : 'sign-in'}`)}>{isSignInPage ? "Sign-up" : "Sign-in"}</span></div>
+        </div>
     </div>);
 }
 
